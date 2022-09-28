568a6fbe2716a085994164258a6fe325b0b06a1e
ALiSQATEAM <113972332+ALiSQATEAM@users.noreply.github.com>
2022-09-28 13:59:17 +0530

    Update GetClass2.java

--- src/getPackage/GetClass2.java
+++ src/getPackage/GetClass2.java
@@ -4,8 +4,10 @@ public class GetClass2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     System.out.print("Git");
-		System.out.print("ADMIN");
+        System.out.print("Git");
+        System.out.print("ADMIN");
+        System.out.print("pull");
+		
 	}
 
 }
