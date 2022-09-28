0d25726577ae324c4c78c0916e005be0bd800b6c
hoshiars <hoshiars@AithGGN1357.del.aithent.com>
2022-09-28 13:57:57 +0530

    remove space

--- src/getPackage/GetClass.java
+++ src/getPackage/GetClass.java
@@ -5,15 +5,13 @@ public class GetClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      System.out.print("Git");
-<<<<<<< HEAD
+
      System.out.print("eclipse");
-=======
      System.out.print("Git");
      System.out.print("Git3");
      System.out.print("inssert new line");
      System.out.print("LEAD ADD NEW LINE");
     
->>>>>>> branch 'master' of https://github.com/ALiSQATEAM/GitDemoProject.git
 	}
 
 }
